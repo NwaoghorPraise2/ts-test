2e8104eb4d165adec2648e40e0a91f8a1bf09990
Nwaoghor Praise Chukunweiken <nwaoghorpraise@admins-MacBook-Air.local>
2024-06-15 17:45:54 +0100

    Init

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
-console.log("Hello World");
-console.log("Coding Guru");
+class User {
+    constructor(name, age) {
+        this.city = 'Delhi';
+        this.name = name;
+        this.age = age;
+    }
+}
+const user = new User('Rahul', 25);
+user.city = 'Mumbai';
+//Praive and public
+class User2 {
+    constructor(name, age) {
+        this.city = 'Delhi';
+        this.name = name;
+        this.age = age;
+    }
+}
+// production grade level code
+class User3 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.city = 'Delhi';
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-console.log("Hello World");
-console.log("Coding Guru");
+class User {
+    name: string;
+    age: number;
+    city: string = 'Delhi';
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const user = new User('Rahul', 25);
+
+user.city = 'Mumbai';
+
+
+//Praive and public
+
+class User2 {
+    private name: string;
+    public age: number;
+    city: string = 'Delhi';
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+// production grade level code
+class User3 {
+    readonly city: string = 'Delhi';
+    constructor(private name: string, public age: number) {
+    }
+}
+
